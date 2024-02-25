8a17b3245f34e73c27de5bcd94029d9943c0ad24
prady13 <saxenapradyumn@gmail.com>
2024-02-25 16:14:51 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Pradyumn Saxena on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
